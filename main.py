c4dfe25746c42a495076b12f875a104cc533b8c9
Manav Goyal <manavgoyaltheboss@gmail.com>
2023-06-28 18:49:38 +0530

    created basic template

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("index.html")
+
+
+@app.route('/api', methods=['GET', 'POST'])
+def qa():
+    data = ""
+    if request.method == 'POST':
+        return jsonify(data)
+    return jsonify(data)
 
 
 app.run(debug=True)
