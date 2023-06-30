228b8e04043a943af0c024b4bc34720d817b82fe
Manav Goyal <manavgoyaltheboss@gmail.com>
2023-06-30 18:14:51 +0530

    frontend website created

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import requests
 import pickle
 import pandas as pd
+import logging
 
 import sys
 sys.path.append("model")
@@ -13,24 +14,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
-    # It can be accessed by http://127.0.0.1:5000/
-    return HELLO_HTML
-
-
-HELLO_HTML = """
-        <html><body>
-            <h1>Welcome to my api: Whatscooking!</h1>
-            <p>Please add some ingredients to the url to receive recipe recommendations.
-                You can do this by appending "/recipe?ingredients= Pasta Tomato ..." to the current url.
-            <br>Click <a href="/recipe?ingredients= pasta tomato onion">here</a> for an example when using the ingredients: pasta, tomato and onion.
-        </body></html>
-     """
+    return render_template("index.html")
 
 
 @app.route('/recipe', methods=["GET", "POST"])
 def recipe():
     ingredients = request.args.get('ingredients')
     recipe = recommendation.RecSys(ingredients)
+    # logging.warning('This is a debug message')
+    # logging.warning(ingredients)
 
     # We need to turn output into JSON.
     response = {}
@@ -53,5 +45,9 @@ if __name__ == "__main__":
 # Extracting Features: TF-IDF (TfidfVectorizer)
 # Recommendation System: Cosine similarity
 
+# http://127.0.0.1:5000/recipe?ingredients=%20pasta%20tomato%20onion
+
+# Display data in result.html
+# ast library error
 # Take a picture of the ingredients and extract data
 # Write a blog on this project
